4b808ec1fc46e8d834426359548c9b3fcba0119b
Anh Quan <dangquanuet@gmail.com>
2020-05-26 13:14:36 +0700

    Refactor, add shared_prefs

---
+++ lib/provider.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// provider sample
+class ProviderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProviderPage(),
+    );
+  }
+}
+
+class ProviderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("Scaffold");
+    return Scaffold(
+      body: Center(
+        child: Consumer<CountModel>(builder: (context, countModel, child) {
+          print("Consumer ${countModel.count}");
+          return GestureDetector(
+            onTap: () => getCountModel(context).decreaseCounter(),
+            child: Text(
+              countModel.count.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          );
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => getCountModel(context).increaseCounter(),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  CountModel getCountModel(context) =>
+      Provider.of<CountModel>(context, listen: false);
+}
+
+class CountModel with ChangeNotifier {
+  static const COUNTER_KEY = "COUNTER";
+  int count;
+
+  CountModel() {
+    _getCounter();
+  }
+
+  _getCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getInt(COUNTER_KEY);
+    if (value != null) {
+      count = value;
+    } else {
+      count = 0;
+    }
+    notifyListeners();
+  }
+
+  increaseCounter() async {
+    count++;
+    notifyListeners();
+    SharedPreferences.getInstance()
+        .then((value) => {value.setInt(COUNTER_KEY, count)});
+  }
+
+  decreaseCounter() async {
+    count--;
+    notifyListeners();
+    SharedPreferences.getInstance()
+        .then((value) => {value.setInt(COUNTER_KEY, count)});
+  }
+}
---
+++ lib/state.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+/// setState sample
+class StateApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: StatePage(),
+    );
+  }
+}
+
+class StatePage extends StatefulWidget {
+  @override
+  _StatePageState createState() => _StatePageState();
+}
+
+class _StatePageState extends State<StatePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("Scaffold $_counter");
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
