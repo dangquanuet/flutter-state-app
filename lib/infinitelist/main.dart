374ae3a2af74f50ff007019903a6d4bda21b8a53
Anh Quan <dangquanuet@gmail.com>
2020-06-10 17:32:21 +0700

    add infinite list

---
+++ lib/infinitelist/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterstateapp/infinitelist/post.dart';
+import 'package:flutterstateapp/infinitelist/post_bloc.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FLutter infinite scroll",
+      home: Scaffold(
+        body: BlocProvider(
+          create: (context) =>
+              PostBLoc(httpClient: http.Client())..add(Fetch()),
+          child: HomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+  PostBLoc _postBLoc;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _postBLoc = BlocProvider.of<PostBLoc>(context);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _postBLoc.add(Fetch());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PostBLoc, PostState>(
+      builder: (context, state) {
+        if (state is PostUninitialized) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (state is PostError) {
+          return Center(
+            child: Text("failed to fetch posts"),
+          );
+        }
+
+        if (state is PostLoaded) {
+          if (state.posts.isEmpty) {
+            return Center(
+              child: Text("no posts"),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return index >= state.posts.length
+                  ? BottomLoader()
+                  : PostWidget(post: state.posts[index]);
+            },
+            itemCount: state.hasReachedMax
+                ? state.posts.length
+                : state.posts.length + 1,
+            controller: _scrollController,
+          );
+        }
+      },
+    );
+  }
+}
+
+class BottomLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Center(
+        child: SizedBox(
+          width: 33,
+          height: 33,
+          child: CircularProgressIndicator(
+            strokeWidth: 1.5,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  final Post post;
+
+  const PostWidget({Key key, @required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(
+        "${post.id}",
+        style: TextStyle(fontSize: 10.0),
+      ),
+      title: Text(post.title),
+      isThreeLine: true,
+      subtitle: Text(post.body),
+      dense: true,
+    );
+  }
+}
+
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+}
---
+++ lib/infinitelist/post_bloc.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutterstateapp/infinitelist/post.dart';
+import 'package:flutterstateapp/infinitelist/post_event.dart';
+import 'package:flutterstateapp/infinitelist/post_state.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
+
+export 'post_event.dart';
+export 'post_state.dart';
+
+class PostBLoc extends Bloc<PostEvent, PostState> {
+  final http.Client httpClient;
+
+  PostBLoc({@required this.httpClient});
+
+  @override
+  PostState get initialState => PostUninitialized();
+
+  @override
+  Stream<Transition<PostEvent, PostState>> transformEvents(
+      Stream<PostEvent> events, transitionFn) {
+    return super.transformEvents(
+        events.debounceTime(const Duration(milliseconds: 500)), transitionFn);
+  }
+
+  @override
+  Stream<PostState> mapEventToState(PostEvent event) async* {
+    final currentState = state;
+    if (event is Fetch && !_hasReachedMax(currentState)) {
+      try {
+        if (currentState is PostUninitialized) {
+          final posts = await _fetchPosts(0, 20);
+          yield PostLoaded(posts: posts, hasReachedMax: false);
+          return;
+        }
+        if (currentState is PostLoaded) {
+          final posts = await _fetchPosts(currentState.posts.length, 20);
+          yield posts.isEmpty
+              ? currentState.copyWith(hasReachedMax: true)
+              : PostLoaded(
+                  posts: currentState.posts + posts, hasReachedMax: false);
+        }
+      } catch (_) {
+        yield PostError();
+      }
+    }
+  }
+
+  bool _hasReachedMax(PostState state) =>
+      state is PostLoaded && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get(
+        "https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit");
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+      return data.map((rawPost) {
+        return Post(
+          id: rawPost["id"],
+          title: rawPost["title"],
+          body: rawPost["body"],
+        );
+      }).toList();
+    } else {
+      throw Exception("error fetching posts");
+    }
+  }
+}
